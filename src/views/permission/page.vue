784389a6f1cabdafe6e2b078993159341f972bd8
chenyayong <451998731@qq.com>
2021-07-02 04:56:06 +0800

    Update page.vue

--- src/views/permission/page.vue
+++ src/views/permission/page.vue
@@ -1,24 +1,14 @@
 <template>
   <div class="app-container">
-    <switch-roles @change="handleRolesChange" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import SwitchRoles from './components/SwitchRoles.vue'
 
 @Component({
-  name: 'PagePermission',
-  components: {
-    SwitchRoles
-  }
+  name: 'PagePermission'
 })
 export default class extends Vue {
-  private handleRolesChange() {
-    this.$router.push({ path: '/permission/index?' + +new Date() }).catch(err => {
-      console.warn(err)
-    })
-  }
 }
 </script>
