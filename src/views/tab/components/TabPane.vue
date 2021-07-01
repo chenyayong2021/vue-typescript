d70f5998168989b7c5d65860f9ec91c18253933d
hello <451998731@qq.com>
2021-07-02 02:54:57 +0800

    tab业务完成

--- src/views/tab/components/TabPane.vue
+++ src/views/tab/components/TabPane.vue
@@ -1,11 +1,5 @@
 <template>
-  <el-table
-    :data="list"
-    border
-    fit
-    highlight-current-row
-    style="width: 100%"
-  >
+  <el-table :data="list" border fit highlight-current-row style="width: 100%">
     <el-table-column
       v-loading="loading"
       align="center"
@@ -13,70 +7,44 @@
       width="65"
       element-loading-text="请给我点时间！"
     >
-      <template slot-scope="{row}">
+      <template slot-scope="{ row }">
         <span>{{ row.id }}</span>
       </template>
     </el-table-column>
 
-    <el-table-column
-      width="180px"
-      align="center"
-      label="Date"
-    >
-      <template slot-scope="{row}">
+    <el-table-column width="180px" align="center" label="Date">
+      <template slot-scope="{ row }">
         <span>{{ row.timestamp | parseTime }}</span>
       </template>
     </el-table-column>
 
-    <el-table-column
-      min-width="240px"
-      label="Title"
-    >
-      <template slot-scope="{row}">
+    <el-table-column min-width="240px" label="Title">
+      <template slot-scope="{ row }">
         <span>{{ row.title }}</span>
         <el-tag>{{ row.type }}</el-tag>
       </template>
     </el-table-column>
 
-    <el-table-column
-      width="180px"
-      align="center"
-      label="Author"
-    >
-      <template slot-scope="{row}">
+    <el-table-column width="180px" align="center" label="Author">
+      <template slot-scope="{ row }">
         <span>{{ row.author }}</span>
       </template>
     </el-table-column>
 
-    <el-table-column
-      width="120px"
-      label="Importance"
-    >
-      <template slot-scope="{row}">
-        <svg-icon
-          v-for="n in +row.importance"
-          :key="n"
-          name="star"
-        />
+    <el-table-column width="120px" label="Importance">
+      <template slot-scope="{ row }">
+        <svg-icon v-for="n in +row.importance" :key="n" name="star" />
       </template>
     </el-table-column>
 
-    <el-table-column
-      align="center"
-      label="Readings"
-      width="95"
-    >
-      <template slot-scope="{row}">
+    <el-table-column align="center" label="Readings" width="95">
+      <template slot-scope="{ row }">
         <span>{{ row.pageviews }}</span>
       </template>
     </el-table-column>
 
-    <el-table-column
-      class-name="status-col"
-      label="Status"
-      width="110"
-    >
-      <template slot-scope="{row}">
+    <el-table-column class-name="status-col" label="Status" width="110">
+      <template slot-scope="{ row }">
         <el-tag :type="row.status | articleStatusFilter">
           {{ row.status }}
         </el-tag>
